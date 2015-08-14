a8aa9a90e474798fef5c76c7b870296a1cd59bc1
Ratchaphon Manosuthi <themelo64@gmail.com>
2015-08-14 16:19:20 +0700

    Update disable-DHCP.sh

--- disable-DHCP.sh
+++ disable-DHCP.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
+#
 # Patch for unwanted DHCP on boot
+#
 # Location: /etc/systemd/network/eth0.network
+#
 # Explaination: On boot, the device will attempt to send a DHCP request,
 # even though netctl (or the user) explicitly stated that a static IP be used.
+#
 # Side effects:
 # - Unexplained connection failures
 # - Dual IP (may cause problems with some router, e.g. the horrible Huawei HG8245 I'm using)
 # - Dual MAC addresses
+# - Port forwarding failures (unconfirmed)
 
 sed -i 's/DHCP=yes/DHCP=no/g' /etc/systemd/network/eth0.network
